74597b4c27c1f03237b772fecbd6aea248787f98
hygge18 <112929845+handingna@users.noreply.github.com>
2023-11-01 17:05:11 +0800

    change be/model/user.py

--- be/model/user.py
+++ be/model/user.py
@@ -1,9 +1,8 @@
+from be.model import error
+from be.model.db_conn import DBConn
 import jwt
 import time
 import logging
-import pymongo  # 导入MongoDB库
-from be.model import error
-from be.model import db_conn
 
 # encode a json string like:
 #   {
@@ -11,8 +10,6 @@ from be.model import db_conn
 #       "terminal": [terminal code],
 #       "timestamp": [ts]} to a JWT
 #   }
-
-
 def jwt_encode(user_id: str, terminal: str) -> str:
     encoded = jwt.encode(
         {"user_id": user_id, "terminal": terminal, "timestamp": time.time()},
@@ -22,20 +19,46 @@ def jwt_encode(user_id: str, terminal: str) -> str:
     return encoded.encode("utf-8").decode("utf-8")
 
 
-class User(db_conn.DBConn):
-    token_lifetime: int = 3600  # 3600 seconds
+# decode a JWT to a json string like:
+#   {
+#       "user_id": [user name],
+#       "terminal": [terminal code],
+#       "timestamp": [ts]} to a JWT
+#   }
+def jwt_decode(encoded_token, user_id: str) -> str:
+    decoded = jwt.decode(encoded_token, key=user_id, algorithms="HS256")
+    return decoded
 
-    def __init__(self, db_url, db_name):
-        super().__init__(db_url, db_name)
-        self.collection = self.conn['user']
 
-    # ...
-    #用户注册
+class User(DBConn):
+    token_lifetime: int = 3600  # 3600 second
+
+    def __init__(self):
+        DBConn.__init__(self)
+
+    # 检查用户 token
+    def __check_token(self, user_id, db_token, token) -> bool:
+        try:
+            if db_token != token:
+                return False
+            jwt_text = jwt_decode(encoded_token=token, user_id=user_id)
+            ts = jwt_text["timestamp"]
+            if ts is not None:
+                now = time.time()
+                if self.token_lifetime > now - ts >= 0:
+                    return True
+        except jwt.exceptions.InvalidSignatureError as e:
+            logging.error(str(e))
+            return False
+
+    # 注册用户
     def register(self, user_id: str, password: str):
         try:
+            # 生成用户的 token
             terminal = "terminal_{}".format(str(time.time()))
             token = jwt_encode(user_id, terminal)
 
+            # 创建用户数据并插入 MongoDB
             user_data = {
                 "user_id": user_id,
                 "password": password,
@@ -43,111 +66,269 @@ class User(db_conn.DBConn):
                 "token": token,
                 "terminal": terminal
             }
-            self.collection.insert_one(user_data)
-        except pymongo.errors.DuplicateKeyError:
-            return error.error_exist_user_id(user_id)
+            self.db.db.user.insert_one(user_data)
+        except Exception as e:
+            return 530, "{}".format(str(e))
         return 200, "ok"
 
-
-    #检查用户令牌的有效性，确保令牌与数据库中存储的一致，并且令牌在有效期内。
+    # 检查用户 token
     def check_token(self, user_id: str, token: str) -> (int, str):
-        user_data = self.collection.find_one({"user_id": user_id})
-        if user_data is None:
+        user = self.db.db.user.find_one({"user_id": user_id})
+        if user is None:
             return error.error_authorization_fail()
-        db_token = user_data["token"]
+
+        db_token = user.get("token", "")
         if not self.__check_token(user_id, db_token, token):
             return error.error_authorization_fail()
         return 200, "ok"
 
-    #检查用户密码的有效性，验证提供的密码与数据库中存储的密码是否一致。
+    # 检查用户密码
     def check_password(self, user_id: str, password: str) -> (int, str):
-        user_data = self.collection.find_one({"user_id": user_id})
-        if user_data is None:
+        user = self.db.db.user.find_one({"user_id": user_id})
+        if user is None:
             return error.error_authorization_fail()
 
-        if password != user_data["password"]:
+        db_password = user.get("password", "")
+        if password != db_password:
             return error.error_authorization_fail()
 
         return 200, "ok"
 
-    #用户登录方法，检查用户密码
+    # 用户登录
     def login(self, user_id: str, password: str, terminal: str) -> (int, str, str):
-        token = ""
         try:
             code, message = self.check_password(user_id, password)
             if code != 200:
                 return code, message, ""
 
+            # 更新用户的 token
             token = jwt_encode(user_id, terminal)
-
-            self.collection.update_one(
-                {"user_id": user_id},
-                {"$set": {"token": token, "terminal": terminal}}
-            )
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e)), ""
-        except BaseException as e:
+            self.db.db.user.update_one({"user_id": user_id}, {"$set": {"token": token, "terminal": terminal}})
+        except Exception as e:
             return 530, "{}".format(str(e)), ""
         return 200, "ok", token
 
-    #用户登出方法
-    def logout(self, user_id: str, token: str) -> (int, str):
+    # 用户登出
+    def logout(self, user_id: str, token: str) -> bool:
         try:
             code, message = self.check_token(user_id, token)
             if code != 200:
                 return code, message
 
+            # 生成新的 token
             terminal = "terminal_{}".format(str(time.time()))
             dummy_token = jwt_encode(user_id, terminal)
 
-            self.collection.update_one(
-                {"user_id": user_id},
-                {"$set": {"token": dummy_token, "terminal": terminal}}
-            )
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
+            # 更新用户的 token
+            self.db.db.user.update_one({"user_id": user_id}, {"$set": {"token": dummy_token, "terminal": terminal}})
+        except Exception as e:
             return 530, "{}".format(str(e))
         return 200, "ok"
 
-    #用户注销方法，从数据库中删除用户的信息，实现用户注销操作
+    # 注销用户
     def unregister(self, user_id: str, password: str) -> (int, str):
         try:
             code, message = self.check_password(user_id, password)
             if code != 200:
                 return code, message
 
-            result = self.collection.delete_one({"user_id": user_id})
+            # 删除用户数据
+            result = self.db.db.user.delete_one({"user_id": user_id})
             if result.deleted_count == 1:
                 return 200, "ok"
-            else:
-                return error.error_authorization_fail()
-
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
+        except Exception as e:
             return 530, "{}".format(str(e))
-        return 200, "ok"
+        return error.error_authorization_fail()
 
-    #首先检查旧密码是否正确，然后生成新的 JWT 令牌，并将新密码、令牌和终端信息更新到 MongoDB 数据库中
-    def change_password(self, user_id: str, old_password: str, new_password: str) -> (int, str):
+    # 修改密码
+    def change_password(self, user_id: str, old_password: str, new_password: str) -> bool:
         try:
             code, message = self.check_password(user_id, old_password)
             if code != 200:
                 return code, message
 
+            # 更新密码和 token
             terminal = "terminal_{}".format(str(time.time()))
             token = jwt_encode(user_id, terminal)
-
-            # 更新用户的密码、令牌和终端信息
-            self.collection.update_one(
-                {"user_id": user_id},
-                {"$set": {"password": new_password, "token": token, "terminal": terminal}}
-            )
-
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
-            return 530, "{}".format(str(e)), ""
-
+            self.db.db.user.update_one({"user_id": user_id},
+                                       {"$set": {"password": new_password, "token": token, "terminal": terminal}})
+        except Exception as e:
+            return 530, "{}".format(str(e))
         return 200, "ok"
+
+# import jwt
+# import time
+# import logging
+# import sqlite3 as sqlite
+# from be.model import error
+# from be.model import db_conn
+#
+# # encode a json string like:
+# #   {
+# #       "user_id": [user name],
+# #       "terminal": [terminal code],
+# #       "timestamp": [ts]} to a JWT
+# #   }
+#
+#
+# def jwt_encode(user_id: str, terminal: str) -> str:
+#     encoded = jwt.encode(
+#         {"user_id": user_id, "terminal": terminal, "timestamp": time.time()},
+#         key=user_id,
+#         algorithm="HS256",
+#     )
+#     return encoded.encode("utf-8").decode("utf-8")
+#
+#
+# # decode a JWT to a json string like:
+# #   {
+# #       "user_id": [user name],
+# #       "terminal": [terminal code],
+# #       "timestamp": [ts]} to a JWT
+# #   }
+# def jwt_decode(encoded_token, user_id: str) -> str:
+#     decoded = jwt.decode(encoded_token, key=user_id, algorithms="HS256")
+#     return decoded
+#
+#
+# class User(db_conn.DBConn):
+#     token_lifetime: int = 3600  # 3600 second
+#
+#     def __init__(self):
+#         db_conn.DBConn.__init__(self)
+#
+#     def __check_token(self, user_id, db_token, token) -> bool:
+#         try:
+#             if db_token != token:
+#                 return False
+#             jwt_text = jwt_decode(encoded_token=token, user_id=user_id)
+#             ts = jwt_text["timestamp"]
+#             if ts is not None:
+#                 now = time.time()
+#                 if self.token_lifetime > now - ts >= 0:
+#                     return True
+#         except jwt.exceptions.InvalidSignatureError as e:
+#             logging.error(str(e))
+#             return False
+#
+#     def register(self, user_id: str, password: str):
+#         try:
+#             terminal = "terminal_{}".format(str(time.time()))
+#             token = jwt_encode(user_id, terminal)
+#             self.conn.execute(
+#                 "INSERT into user(user_id, password, balance, token, terminal) "
+#                 "VALUES (?, ?, ?, ?, ?);",
+#                 (user_id, password, 0, token, terminal),
+#             )
+#             self.conn.commit()
+#         except sqlite.Error:
+#             return error.error_exist_user_id(user_id)
+#         return 200, "ok"
+#
+#     def check_token(self, user_id: str, token: str) -> (int, str):
+#         cursor = self.conn.execute("SELECT token from user where user_id=?", (user_id,))
+#         row = cursor.fetchone()
+#         if row is None:
+#             return error.error_authorization_fail()
+#         db_token = row[0]
+#         if not self.__check_token(user_id, db_token, token):
+#             return error.error_authorization_fail()
+#         return 200, "ok"
+#
+#     def check_password(self, user_id: str, password: str) -> (int, str):
+#         cursor = self.conn.execute(
+#             "SELECT password from user where user_id=?", (user_id,)
+#         )
+#         row = cursor.fetchone()
+#         if row is None:
+#             return error.error_authorization_fail()
+#
+#         if password != row[0]:
+#             return error.error_authorization_fail()
+#
+#         return 200, "ok"
+#
+#     def login(self, user_id: str, password: str, terminal: str) -> (int, str, str):
+#         token = ""
+#         try:
+#             code, message = self.check_password(user_id, password)
+#             if code != 200:
+#                 return code, message, ""
+#
+#             token = jwt_encode(user_id, terminal)
+#             cursor = self.conn.execute(
+#                 "UPDATE user set token= ? , terminal = ? where user_id = ?",
+#                 (token, terminal, user_id),
+#             )
+#             if cursor.rowcount == 0:
+#                 return error.error_authorization_fail() + ("",)
+#             self.conn.commit()
+#         except sqlite.Error as e:
+#             return 528, "{}".format(str(e)), ""
+#         except BaseException as e:
+#             return 530, "{}".format(str(e)), ""
+#         return 200, "ok", token
+#
+#     def logout(self, user_id: str, token: str) -> bool:
+#         try:
+#             code, message = self.check_token(user_id, token)
+#             if code != 200:
+#                 return code, message
+#
+#             terminal = "terminal_{}".format(str(time.time()))
+#             dummy_token = jwt_encode(user_id, terminal)
+#
+#             cursor = self.conn.execute(
+#                 "UPDATE user SET token = ?, terminal = ? WHERE user_id=?",
+#                 (dummy_token, terminal, user_id),
+#             )
+#             if cursor.rowcount == 0:
+#                 return error.error_authorization_fail()
+#
+#             self.conn.commit()
+#         except sqlite.Error as e:
+#             return 528, "{}".format(str(e))
+#         except BaseException as e:
+#             return 530, "{}".format(str(e))
+#         return 200, "ok"
+#
+#     def unregister(self, user_id: str, password: str) -> (int, str):
+#         try:
+#             code, message = self.check_password(user_id, password)
+#             if code != 200:
+#                 return code, message
+#
+#             cursor = self.conn.execute("DELETE from user where user_id=?", (user_id,))
+#             if cursor.rowcount == 1:
+#                 self.conn.commit()
+#             else:
+#                 return error.error_authorization_fail()
+#         except sqlite.Error as e:
+#             return 528, "{}".format(str(e))
+#         except BaseException as e:
+#             return 530, "{}".format(str(e))
+#         return 200, "ok"
+#
+#     def change_password(
+#         self, user_id: str, old_password: str, new_password: str
+#     ) -> bool:
+#         try:
+#             code, message = self.check_password(user_id, old_password)
+#             if code != 200:
+#                 return code, message
+#
+#             terminal = "terminal_{}".format(str(time.time()))
+#             token = jwt_encode(user_id, terminal)
+#             cursor = self.conn.execute(
+#                 "UPDATE user set password = ?, token= ? , terminal = ? where user_id = ?",
+#                 (new_password, token, terminal, user_id),
+#             )
+#             if cursor.rowcount == 0:
+#                 return error.error_authorization_fail()
+#
+#             self.conn.commit()
+#         except sqlite.Error as e:
+#             return 528, "{}".format(str(e))
+#         except BaseException as e:
+#             return 530, "{}".format(str(e))
+#         return 200, "ok"
