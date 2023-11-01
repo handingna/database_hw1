a39b0fa0ea4122efcc9436506edcfc73cf3ee4b1
GuanZhuang10 <105870869+GuanZhuang10@users.noreply.github.com>
2023-11-01 11:14:33 +0800

    Add files via upload

--- be/model/buyer.py
+++ be/model/buyer.py
@@ -1,189 +1,160 @@
-import sqlite3 as sqlite
 import uuid
 import json
 import logging
+from pymongo import MongoClient
 from be.model import db_conn
 from be.model import error
 
 
 class Buyer(db_conn.DBConn):
     def __init__(self):
-        db_conn.DBConn.__init__(self)
+        super().__init__()  # 初始化Buyer类，继承了数据库连接类DBConn
 
-    def new_order(
-        self, user_id: str, store_id: str, id_and_count: [(str, int)]
-    ) -> (int, str, str):
-        order_id = ""
+    # 创建新订单方法
+    def new_order(self, user_id: str, store_id: str, id_and_count: [(str, int)]) -> (int, str, str):
+        order_id = ""  # 初始化订单ID为空字符串
         try:
+            # 检查用户ID是否存在
             if not self.user_id_exist(user_id):
-                return error.error_non_exist_user_id(user_id) + (order_id,)
+                return error.error_non_exist_user_id(user_id) + (order_id,)  # 返回用户ID不存在的错误信息
+
+            # 检查商店ID是否存在
             if not self.store_id_exist(store_id):
-                return error.error_non_exist_store_id(store_id) + (order_id,)
-            uid = "{}_{}_{}".format(user_id, store_id, str(uuid.uuid1()))
+                return error.error_non_exist_store_id(store_id) + (order_id,)  # 返回商店ID不存在的错误信息
 
+            uid = "{}_{}_{}".format(user_id, store_id, str(uuid.uuid1()))  # 生成订单ID，使用UUID
+
+            # 遍历传入的图书ID和购买数量
             for book_id, count in id_and_count:
-                cursor = self.conn.execute(
-                    "SELECT book_id, stock_level, book_info FROM store "
-                    "WHERE store_id = ? AND book_id = ?;",
-                    (store_id, book_id),
+                # 查询图书库存和信息
+                store_data = self.db.store.find_one(
+                    {"store_id": store_id, "books.book_id": book_id}
                 )
-                row = cursor.fetchone()
-                if row is None:
-                    return error.error_non_exist_book_id(book_id) + (order_id,)
+                if store_data is None:
+                    return error.error_non_exist_book_id(book_id) + (order_id,)  # 返回图书ID不存在的错误信息
 
-                stock_level = row[1]
-                book_info = row[2]
-                book_info_json = json.loads(book_info)
-                price = book_info_json.get("price")
+                book_info = next(
+                    (book for book in store_data["books"] if book["book_id"] == book_id),
+                    None,
+                )
+                stock_level = book_info["stock_level"]
+                price = book_info["price"]
 
+                # 检查库存是否足够
                 if stock_level < count:
-                    return error.error_stock_level_low(book_id) + (order_id,)
+                    return error.error_stock_level_low(book_id) + (order_id,)  # 返回库存不足的错误信息
 
-                cursor = self.conn.execute(
-                    "UPDATE store set stock_level = stock_level - ? "
-                    "WHERE store_id = ? and book_id = ? and stock_level >= ?; ",
-                    (count, store_id, book_id, count),
-                )
-                if cursor.rowcount == 0:
-                    return error.error_stock_level_low(book_id) + (order_id,)
-
-                self.conn.execute(
-                    "INSERT INTO new_order_detail(order_id, book_id, count, price) "
-                    "VALUES(?, ?, ?, ?);",
-                    (uid, book_id, count, price),
+                # 更新图书库存
+                self.db.store.update_one(
+                    {"store_id": store_id, "books.book_id": book_id},
+                    {"$inc": {"books.$.stock_level": -count}},
                 )
 
-            self.conn.execute(
-                "INSERT INTO new_order(order_id, store_id, user_id) "
-                "VALUES(?, ?, ?);",
-                (uid, store_id, user_id),
+                # 插入新订单的详细信息
+                self.db.new_order_detail.insert_one(
+                    {"order_id": uid, "book_id": book_id, "count": count, "price": price}
+                )
+
+            # 插入新订单
+            self.db.new_order.insert_one(
+                {"order_id": uid, "store_id": store_id, "user_id": user_id}
             )
-            self.conn.commit()
-            order_id = uid
-        except sqlite.Error as e:
-            logging.info("528, {}".format(str(e)))
-            return 528, "{}".format(str(e)), ""
-        except BaseException as e:
-            logging.info("530, {}".format(str(e)))
-            return 530, "{}".format(str(e)), ""
+            order_id = uid  # 更新订单ID
+        except Exception as e:
+            logging.info("Error: {}".format(str(e)))
+            return 500, "Internal Server Error", ""
 
-        return 200, "ok", order_id
+        return 200, "ok", order_id  # 返回成功状态码、消息和订单ID
 
+    # 支付订单方法
     def payment(self, user_id: str, password: str, order_id: str) -> (int, str):
-        conn = self.conn
         try:
-            cursor = conn.execute(
-                "SELECT order_id, user_id, store_id FROM new_order WHERE order_id = ?",
-                (order_id,),
-            )
-            row = cursor.fetchone()
-            if row is None:
-                return error.error_invalid_order_id(order_id)
+            # 查询订单信息
+            order_data = self.db.new_order.find_one({"order_id": order_id})
+            if order_data is None:
+                return error.error_invalid_order_id(order_id)  # 返回无效订单ID的错误信息
 
-            order_id = row[0]
-            buyer_id = row[1]
-            store_id = row[2]
+            buyer_id = order_data["user_id"]
+            store_id = order_data["store_id"]
 
+            # 检查用户权限
             if buyer_id != user_id:
-                return error.error_authorization_fail()
+                return error.error_authorization_fail()  # 返回权限验证失败的错误信息
 
-            cursor = conn.execute(
-                "SELECT balance, password FROM user WHERE user_id = ?;", (buyer_id,)
-            )
-            row = cursor.fetchone()
-            if row is None:
-                return error.error_non_exist_user_id(buyer_id)
-            balance = row[0]
-            if password != row[1]:
-                return error.error_authorization_fail()
+            # 查询用户余额和密码
+            user_data = self.db.user.find_one({"user_id": buyer_id})
+            if user_data is None:
+                return error.error_non_exist_user_id(buyer_id)  # 返回用户ID不存在的错误信息
 
-            cursor = conn.execute(
-                "SELECT store_id, user_id FROM user_store WHERE store_id = ?;",
-                (store_id,),
-            )
-            row = cursor.fetchone()
-            if row is None:
-                return error.error_non_exist_store_id(store_id)
+            balance = user_data["balance"]
+            if password != user_data["password"]:
+                return error.error_authorization_fail()  # 返回权限验证失败的错误信息
 
-            seller_id = row[1]
+            # 查询商店信息
+            store_data = self.db.user_store.find_one({"store_id": store_id})
+            if store_data is None:
+                return error.error_non_exist_store_id(store_id)  # 返回商店ID不存在的错误信息
 
+            seller_id = store_data["user_id"]
+
+            # 检查卖家ID是否存在
             if not self.user_id_exist(seller_id):
-                return error.error_non_exist_user_id(seller_id)
+                return error.error_non_exist_user_id(seller_id)  # 返回卖家ID不存在的错误信息
 
-            cursor = conn.execute(
-                "SELECT book_id, count, price FROM new_order_detail WHERE order_id = ?;",
-                (order_id,),
-            )
             total_price = 0
-            for row in cursor:
-                count = row[1]
-                price = row[2]
-                total_price = total_price + price * count
 
+            # 查询订单详细信息并计算总价
+            order_details = self.db.new_order_detail.find({"order_id": order_id})
+            for detail in order_details:
+                count = detail["count"]
+                price = detail["price"]
+                total_price += price * count  # 计算订单总价
+
+            # 检查用户余额是否足够支付订单
             if balance < total_price:
-                return error.error_not_sufficient_funds(order_id)
+                return error.error_not_sufficient_funds(order_id)  # 返回余额不足的错误信息
 
-            cursor = conn.execute(
-                "UPDATE user set balance = balance - ?"
-                "WHERE user_id = ? AND balance >= ?",
-                (total_price, buyer_id, total_price),
-            )
-            if cursor.rowcount == 0:
-                return error.error_not_sufficient_funds(order_id)
-
-            cursor = conn.execute(
-                "UPDATE user set balance = balance + ?" "WHERE user_id = ?",
-                (total_price, buyer_id),
+            # 更新买家余额
+            self.db.user.update_one(
+                {"user_id": buyer_id, "balance": {"$gte": total_price}},
+                {"$inc": {"balance": -total_price}},
             )
 
-            if cursor.rowcount == 0:
-                return error.error_non_exist_user_id(buyer_id)
-
-            cursor = conn.execute(
-                "DELETE FROM new_order WHERE order_id = ?", (order_id,)
+            # 更新卖家余额
+            self.db.user.update_one(
+                {"user_id": buyer_id},
+                {"$inc": {"balance": total_price}},
             )
-            if cursor.rowcount == 0:
-                return error.error_invalid_order_id(order_id)
 
-            cursor = conn.execute(
-                "DELETE FROM new_order_detail where order_id = ?", (order_id,)
-            )
-            if cursor.rowcount == 0:
-                return error.error_invalid_order_id(order_id)
+            # 删除订单和订单详细信息
+            self.db.new_order.delete_one({"order_id": order_id})
+            self.db.new_order_detail.delete_many({"order_id": order_id})
 
-            conn.commit()
+        except Exception as e:
+            logging.info("Error: {}".format(str(e)))
+            return 500, "Internal Server Error"
 
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
+        return 200, "ok"  # 返回成功状态码和消息
 
-        except BaseException as e:
-            return 530, "{}".format(str(e))
-
-        return 200, "ok"
-
+    # 用户充值方法
     def add_funds(self, user_id, password, add_value) -> (int, str):
         try:
-            cursor = self.conn.execute(
-                "SELECT password  from user where user_id=?", (user_id,)
+            # 查询用户密码
+            user_data = self.db.user.find_one({"user_id": user_id})
+            if user_data is None:
+                return error.error_authorization_fail()  # 返回权限验证失败的错误信息
+
+            # 检查用户密码是否匹配
+            if user_data["password"] != password:
+                return error.error_authorization_fail()  # 返回权限验证失败的错误信息
+
+            # 更新用户余额
+            self.db.user.update_one(
+                {"user_id": user_id},
+                {"$inc": {"balance": add_value}},
             )
-            row = cursor.fetchone()
-            if row is None:
-                return error.error_authorization_fail()
+        except Exception as e:
+            logging.info("Error: {}".format(str(e)))
+            return 500, "Internal Server Error"
 
-            if row[0] != password:
-                return error.error_authorization_fail()
-
-            cursor = self.conn.execute(
-                "UPDATE user SET balance = balance + ? WHERE user_id = ?",
-                (add_value, user_id),
-            )
-            if cursor.rowcount == 0:
-                return error.error_non_exist_user_id(user_id)
-
-            self.conn.commit()
-        except sqlite.Error as e:
-            return 528, "{}".format(str(e))
-        except BaseException as e:
-            return 530, "{}".format(str(e))
-
-        return 200, "ok"
+        return 200, "ok"  # 返回成功状态码和消息
