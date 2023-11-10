a7a68b1ce0ab2c10202ff60dd9f454640520079c
hygge18 <112929845+handingna@users.noreply.github.com>
2023-11-10 13:40:30 +0800

    最终版

--- be/view/buyer.py
+++ be/view/buyer.py
@@ -40,3 +40,36 @@ def add_funds():
     b = Buyer()
     code, message = b.add_funds(user_id, password, add_value)
     return jsonify({"message": message}), code
+
+
+@bp_buyer.route("/receive_order", methods=["POST"])
+def receive_order():
+    user_id = request.json.get("user_id")
+    order_id = request.json.get("order_id")
+    b = Buyer()
+    code, message = b.receive_order(user_id, order_id)
+    return jsonify({"message": message}), code
+
+
+@bp_buyer.route("/buyer_order_cancel", methods=["POST"])
+def buyer_order_cancel():
+    user_id = request.json.get("user_id")
+    order_id = request.json.get("order_id")
+    b = Buyer()
+    code, message = b.buyer_order_cancel(user_id, order_id)
+    return jsonify({"message": message}), code
+
+
+@bp_buyer.route("/history_order", methods=["POST"])
+def history_order():
+    user_id = request.json.get("user_id")
+    b = Buyer()
+    code, message, order = b.history_order(user_id)
+    return jsonify({"message": message}), code
+
+
+@bp_buyer.route("/overtime_order_cancel", methods=["POST"])
+def overtime_order_cancel():
+    b = Buyer()
+    code, message = b.overtime_order_cancel()
+    return jsonify({"message": message}), code
